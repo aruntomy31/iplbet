db06e7628078cdd670bde3247928f9affb6fd35b
Lallu Anthoor <lallu.anthoor@sap.com>
2017-03-14 16:01:33 +0530

    fixes in ajax call

--- util.js
+++ util.js
@@ -3,43 +3,53 @@
 
 var User = require('./db/User');
 
-var months = [ "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" ];
+var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 
-module.exports.ensureAuthenticated = function(request, response, next) {
+module.exports.ensureAuthenticated = function (request, response, next) {
     // Check if request contains session user variable
-    if(request.isAuthenticated() === true) {
+    if (request.isAuthenticated() === true) {
         // -> Check if user exists in DB
-        User.find({ id : request.user.id }, function(error, user) {
-            if(error || user.length === 0) { console.log(error); response.redirect('/login'); }
-            else {
+        User.find({
+            id: request.user.id
+        }, function (error, user) {
+            if (error || user.length === 0) {
+                console.log(error);
+                response.redirect('/login');
+            } else {
                 // User Exists
                 user = user[0];
-                if(user.admin === true && request.baseUrl === '/users') response.redirect('/admin');
-                else if(user.admin === false && request.baseUrl === '/admin') response.redirect('/users');
+                if (user.admin === true && request.baseUrl === '/users') response.redirect('/admin');
+                else if (user.admin === false && request.baseUrl === '/admin') response.redirect('/users');
                 else return next();
             }
         });
     } else response.redirect('/login');
 };
 
-module.exports.checkAdmin = function(request, response, next) {
-    User.find({ id: request.user.id, admin: true }, function(error, user) {
-        if(error || user.length === 0) {
+module.exports.checkAdmin = function (request, response, next) {
+    User.find({
+        id: request.user.id,
+        admin: true
+    }, function (error, user) {
+        if (error || user.length === 0) {
             response.redirect('/login');
         } else return next();
     });
 };
 
-module.exports.getReadableFixture = function(date) {
+module.exports.getReadableFixture = function (date) {
     var fixture = {};
+    date = new Date(date);
     var dd = date.getDate();
     dd = dd < 10 ? "0" + dd : dd;
-    fixture.date = dd + "-" + months[date.getMonth()] + "-" + date.getFullYear();
+    var mm = date.getMonth();
+    mm = mm < 10 ? "0" + mm : mm;
+    fixture.date = dd + "-" + mm + "-" + date.getFullYear();
     fixture.time = date.toLocaleTimeString();
     return fixture;
 }
 
-module.exports.getIPLDate = function(string) {
+module.exports.getIPLDate = function (string) {
     var parts = string.split(",");
     var dateParts = parts[0].split("/");
     var dateDD = dateParts[0] < 10 ? "0" + dateParts[0] : dateParts[0];
